3a84931e71b348c888a380cf08b7f95423451ef9
mczhanhong <1291136617@qq.com>
2018-07-17 23:34:22 +0800

    三种无缝滚动写法

--- js/main.js
+++ js/main.js
@@ -7,12 +7,12 @@ var _text = [
     '那一天，知道你要走，我们一句话也没有说，当物业的钟声，敲通离别的心门，却打不开你深深的沉默',
     '我只能深深地祝福你,深深地祝福你,最亲爱的朋友,祝你一路顺风'];
 
-// setInterval实现无缝滚动
-function scroll(dom, text) {
+// setInterval 实现文字水平滚动
+function scrollText1(dom) {
     if (dom.offsetWidth < dom.scrollWidth) {
-        console.log(dom+':文字长度超出了容器宽度');
+        console.log('文字长度超出了容器宽度');
         var wordLength = dom.scrollWidth;//文字长度
-        dom.innerHTML += text;
+        dom.innerHTML += dom.innerHTML;
         var timer = setInterval(function () {
             if (dom.scrollLeft < wordLength) {
                 dom.scrollLeft++;
@@ -20,13 +20,52 @@ function scroll(dom, text) {
                 dom.scrollLeft = 0;
             }
         }, 1000 / 60);
-    } else {
-        console.log(dom+':文字没有超出容器宽度');
+    }else{
+        console.log('文字没有超出容器宽度');
+    }
+}
+
+// setTimeout
+function scrollText2(dom){
+    if(dom.offsetWidth<dom.scrollWidth){
+        console.log('文字长度超出了容器宽度');
+        var wordLength=dom.scrollWidth;
+        dom.innerHTML+=dom.innerHTML;
+        var timer=setTimeout(function fn(){
+            if(dom.scrollLeft<wordLength){
+                dom.scrollLeft++;
+            }else{
+                dom.scrollLeft=0;
+                clearTimeout(timer);
+            }
+            timer=setTimeout(fn,1000/60);
+        },1000/60);
+    }else{
+        console.log('文字没有超出容器宽度');
+    }
+}
+
+// requestAnimationFrame
+function scrollText3(dom){
+    if(dom.offsetWidth<dom.scrollWidth){
+        console.log('文字长度超出了容器宽度');
+        var wordLength=dom.scrollWidth;
+        dom.innerHTML+=dom.innerHTML;
+        var timer=requestAnimationFrame(function fn(){
+            if(dom.scrollLeft<wordLength){
+                dom.scrollLeft++;
+            }else{
+                dom.scrollLeft=0;
+            }
+            timer=requestAnimationFrame(fn);
+        });
+    }else{
+        console.log('文字没有超出容器宽度');
     }
 }
 
 //test
 for (var i = 0; i < text.length; i++) {
     text[i].innerHTML = _text[i%4] + '&nbsp;&nbsp;&nbsp;&nbsp;';
-    scroll(text[i], text[i].innerHTML);
+    scrollText1(text[i]);
 }
